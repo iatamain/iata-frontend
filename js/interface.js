3fa381444641105a0d9ca564545a09e4483fb9c8
Tangerine <30938631+TangerineNe@users.noreply.github.com>
2019-07-26 11:29:57 +0300

    Правки

---
+++ js/interface.js
@@ -0,0 +1,7 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+	document.querySelector("#news-container").setAttribute("class", "news-container-mobile");
+    alert("С мобильного")
+}else{
+	document.querySelector("#news-container").setAttribute("class", "news-container");
+	alert("С пк");
+}
