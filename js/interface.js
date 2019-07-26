015923a62de235655bbb97538ae32dc85298f882
Tangerine <30938631+TangerineNe@users.noreply.github.com>
2019-07-26 11:49:52 +0300

    МП

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,6 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 	document.querySelector("#news-container").setAttribute("class", "news-container-mobile");
-    alert("С мобильного")
+	alert("Зайди с пк, пездюк:D");
 }else{
 	document.querySelector("#news-container").setAttribute("class", "news-container");
-	alert("С пк");
 }
