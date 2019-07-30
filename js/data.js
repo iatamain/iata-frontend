ac38ea10d985a48831f007939b7bc55b19e7c212
Tangerine <30938631+TangerineNe@users.noreply.github.com>
2019-07-31 00:46:57 +0300

    )

--- js/data.js
+++ js/data.js
@@ -55,7 +55,7 @@ if(session.snsName === "vk"){
 			snsPlayerInf.country = data.response[0].country.title;
 			send(snsPlayerInf, "/api/user/update", "put", response=>{
 				let responseObj = response.json()
-				console.log(' .... ', responseObj;
+				console.log(' .... ', responseObj);
 				 responseObj.then(value => {
 					 console.log(value);
 					 console.log(value.loginsCount);
