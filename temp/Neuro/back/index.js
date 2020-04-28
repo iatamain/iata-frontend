81ec4ee0877cc5eb5d9e58890d20b0b5c77e267f
Tangerine <rusy301997@mail.ru>
2020-04-29 00:41:37 +0300

    Явно перечислил заголовки

--- temp/Neuro/back/index.js
+++ temp/Neuro/back/index.js
@@ -31,7 +31,7 @@ app.use(urlencodedParser, (req, res, next)=>{
 	res.header("Access-Control-Allow-Origin", "*");
 //	res.header("Access-Control-Allow-Headers", "Content-Type");
 	res.header("Access-Control-Allow-Methods", "*");
-	res.header("Access-Control-Allow-Headers", "*");
+	res.header("Access-Control-Allow-Headers", "authorization, Origin, Content-type");
 	if(req.method == "OPTIONS"){
 		return res.status(200).send('ok');
 	}
