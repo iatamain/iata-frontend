564069ab8125e81d4e1d1fd9b0328c5b092e4aeb
Max <555659@inbox.ru>
2021-02-18 16:59:35 +0300

    e boi

--- temp/CbossTesting/script.js
+++ temp/CbossTesting/script.js
@@ -1,5 +1,5 @@
 mailru.loader.require('api', function(){
-    mailru.connect.init('783355', '7774e351cde9807b2ab9de47394a8f9f ')
+    mailru.connect.init('783355', '7774e351cde9807b2ab9de47394a8f9f ');
     alert(2);
 });
 
