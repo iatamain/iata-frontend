ad21a32c3a20995425da715ff94fbcbbff712e3e
Max <555659@inbox.ru>
2021-02-18 18:50:47 +0300

    added

--- temp/CbossTesting/script.js
+++ temp/CbossTesting/script.js
@@ -2,7 +2,7 @@ var check = null;
 mailru.loader.require('api', function(){
     mailru.connect.init('783355', '7774e351cde9807b2ab9de47394a8f9f ');
     mailru.common.users.getInfo(function(user_list) {
-        check = user_list;
+        console.log(mailru.session);
         alert(user_list[0].first_name); // выведет имя пользователя с uid 123
       }, '123');
 });
