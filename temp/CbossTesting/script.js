8d38eccf1e9c4ff3187a0dbcd88265de1df585c9
Max <555659@inbox.ru>
2021-02-18 16:56:20 +0300

    edited

--- temp/CbossTesting/script.js
+++ temp/CbossTesting/script.js
@@ -1,6 +1,5 @@
-mailru.loader.require('api', mailAPI);
-mailru.connect.init('783355', '7774e351cde9807b2ab9de47394a8f9f');
-function mailAPI(){
-    alert(1);
-}
+mailru.loader.require('api', function(){
+    alert(2);
+});
+
 console.log('version 0.4');
