5cef8db4d225d3fffb8ec79a76a03bd41556dc0b
Max <555659@inbox.ru>
2021-02-18 16:06:46 +0300

    l

--- temp/CbossTesting/script.js
+++ temp/CbossTesting/script.js
@@ -1,7 +1,10 @@
-let code = window.location.search.substring(1);
-code = JSON.parse('{"' + decodeURI(code).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
-console.log(code.code);
-
+try{
+    let code = window.location.search.substring(1);
+    code = JSON.parse('{"' + decodeURI(code).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+    console.log(code.code);
+    }catch(error){
+        console.log(error);
+    }
     fetch('https://oauth.mail.ru/token', {
         method: 'POST',
         mode: 'cors',
@@ -11,4 +14,4 @@ console.log(code.code);
         'Access-Control-Allow-Origin': 'http://testKsi/'
         }
         })
-console.log('version 0.2');
+console.log('version 0.3');
