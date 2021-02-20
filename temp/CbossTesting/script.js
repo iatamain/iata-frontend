8e3622e38314603f781f3dbca4a2b31f8dca1f58
maksim555659 <555659@inbox.ru>
2021-02-20 13:18:09 +0300

    hot fix

--- temp/CbossTesting/script.js
+++ temp/CbossTesting/script.js
@@ -1,6 +1,6 @@
 var check = null;
 mailru.loader.require('api', function(){
-    mailru.connect.init('783355', '7774e351cde9807b2ab9de47394a8f9f ');
+    mailru.connect.init('783355', '7774e351cde9807b2ab9de47394a8f9f');
     mailru.common.users.getInfo(function(user_list) {
         console.log(mailru.session);
         alert(user_list[0].first_name); // выведет имя пользователя с uid 123
