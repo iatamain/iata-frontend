f190c9806e78ea9b101c4f83e8714bf150735780
Руслан Курочкин <rusy301997@mail.ru>
2021-07-04 22:46:52 +0300

    fixCursor

--- temp/Laplas/frontend/src/components/cursor/index.js
+++ temp/Laplas/frontend/src/components/cursor/index.js
@@ -17,21 +17,27 @@ const Cursor = () => {
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
 		const ctx = canvas.getContext("2d");
-		const handleMouseEnter = ()=>{
+		const handleVisible = () => {
+			cursor.isVisible = true;
+		};
+		const handleHidden = () => {
+			cursor.isVisible = false;
+		};
+		const handleMouseEnter = () => {
 			cursor.isHover = true;
 			console.log("test");
-		}
-		const handleMouseLeave = ()=>{
+		};
+		const handleMouseLeave = () => {
 			cursor.isHover = false;
 			console.log("test2");
-		}
-		const handleMouseDown = ()=>{
+		};
+		const handleMouseDown = () => {
 			cursor.dr = 35;
 			cursor.isActive = true;
 		};
-		const handleMouseUp = ()=>{
+		const handleMouseUp = () => {
 			cursor.isActive = false;
-		}
+		};
 		const handleMouseMove = (event) => {
 			const x = event.clientX;
 			const y = event.clientY;
@@ -41,8 +47,8 @@ const Cursor = () => {
 				cursor.x = x;
 				cursor.y = y;
 			}
-			cursor.dx = (cursor.toX - cursor.x)*10;
-			cursor.dy = (cursor.toY - cursor.y)*10;
+			cursor.dx = (cursor.toX - cursor.x) * 10;
+			cursor.dy = (cursor.toY - cursor.y) * 10;
 		};
 		const cursor = {
 			x: -20,
@@ -58,77 +64,82 @@ const Cursor = () => {
 			minR: 5,
 			isActive: false,
 			isHover: false,
+			isVisible: false,
 		};
 		let id = 0;
-    let last = Date.now();
+		let last = Date.now();
 		function play() {
-      let now = Date.now();
-      let dt = (now - last)/1000;
+			let now = Date.now();
+			let dt = (now - last) / 1000;
 			update(dt);
 			render();
 			id = requestAnimationFrame(play);
-      last = now;
+			last = now;
 		}
 		function update(dt) {
 			const dist = Math.sqrt(
 				(cursor.x - cursor.toX) ** 2 + (cursor.y - cursor.toY) ** 2
 			);
 			const vectSpeed = Math.sqrt(cursor.dx ** 2 + cursor.dy ** 2);
-			if (dist <= vectSpeed*dt) {
+			if (dist <= vectSpeed * dt) {
 				cursor.x = cursor.toX;
 				cursor.y = cursor.toY;
 				cursor.speed = 0;
 			} else {
-				cursor.x += cursor.dx*dt;
-				cursor.y += cursor.dy*dt;
+				cursor.x += cursor.dx * dt;
+				cursor.y += cursor.dy * dt;
 			}
-			cursor.r += cursor.dr*dt;
-			if(cursor.r > cursor.maxR && !cursor.isActive && cursor.dr > 0){
+			cursor.r += cursor.dr * dt;
+			if (cursor.r > cursor.maxR && !cursor.isActive && cursor.dr > 0) {
 				cursor.dr *= -1;
-			}else if(cursor.r > cursor.maxRForActive && cursor.isActive){
+			} else if (cursor.r > cursor.maxRForActive && cursor.isActive) {
 				cursor.r = cursor.maxRForActive;
 			}
-			if(cursor.r < cursor.minR){
+			if (cursor.r < cursor.minR) {
 				cursor.r = cursor.minR;
 				cursor.dr = 0;
 			}
 		}
 		function render() {
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			ctx.fillStyle = "#ABDBE2"; //Рисуем большой круг
-			ctx.strokeStyle = "#ABDBE2";
-			ctx.beginPath();
-			ctx.arc(cursor.x + 5, cursor.y + 5, 25, 0, Math.PI * 2);
-			ctx.closePath();
-			if(!cursor.isHover)ctx.globalAlpha = 0.2;
-			if(!cursor.isHover)ctx.fill();
-			if(cursor.isHover) ctx.stroke();
-			ctx.globalAlpha = 1;
+			if (cursor.isVisible) {
+				ctx.fillStyle = "#ABDBE2"; //Рисуем большой круг
+				ctx.strokeStyle = "#ABDBE2";
+				ctx.beginPath();
+				ctx.arc(cursor.x + 5, cursor.y + 5, 25, 0, Math.PI * 2);
+				ctx.closePath();
+				if (!cursor.isHover) ctx.globalAlpha = 0.2;
+				if (!cursor.isHover) ctx.fill();
+				if (cursor.isHover) ctx.stroke();
+				ctx.globalAlpha = 1;
 
-			ctx.fillStyle = "#ABDBE2"; //Рисуем маленький круг
-			ctx.beginPath();
-			ctx.arc(cursor.toX + 5, cursor.toY + 5, cursor.r, 0, Math.PI * 2);
-			ctx.closePath();
-			ctx.fill();
+				ctx.fillStyle = "#ABDBE2"; //Рисуем маленький круг
+				ctx.beginPath();
+				ctx.arc(cursor.toX + 5, cursor.toY + 5, cursor.r, 0, Math.PI * 2);
+				ctx.closePath();
+				ctx.fill();
+			}
 		}
 		window.addEventListener("mousemove", handleMouseMove);
 		window.addEventListener("mousedown", handleMouseDown);
 		window.addEventListener("mouseup", handleMouseUp);
+		document.addEventListener("mouseenter", handleVisible);
+		document.addEventListener("mouseleave", handleHidden);
 		const listPointerElem = document.querySelectorAll(".cursorPointer");
-		listPointerElem.forEach((value)=>{
+		listPointerElem.forEach((value) => {
 			console.log(value);
 			value.addEventListener("mouseenter", handleMouseEnter);
 			value.addEventListener("mouseleave", handleMouseLeave);
-		})
+		});
 		play();
 		return () => {
 			window.removeEventListener("mousemove", handleMouseMove);
 			window.removeEventListener("mousedown", handleMouseDown);
 			window.removeEventListener("mouseup", handleMouseUp);
-			listPointerElem.forEach((value)=>{
+			listPointerElem.forEach((value) => {
 				value.removeEventListener("mouseenter", handleMouseEnter);
 				value.removeEventListener("mouseleave", handleMouseLeave);
-			})
+			});
 			cancelAnimationFrame(id);
 		};
 	}, [window.innerWidth, window.innerHeight]);
