753032ff1d6442ab00371fe6081ec609ab83c0b9
TangerineNe <rusy301997@mail.ru>
2021-07-10 20:05:44 +0300

    Yazmeya

--- temp/Yazmeyaa/site/source/js/paint.js
+++ temp/Yazmeyaa/site/source/js/paint.js
@@ -1,12 +1,73 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight - 40;
 
 ctx.fillStyle = "black";
 ctx.fillRect(0,0,canvas.width, canvas.height);
 
 var meow = 0;
+
+var clrBtn = document.querySelector(".clearButton");
+clrBtn.onclick = function(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+}
+
+const mouse = {
+    mX : 0,
+    mY : 0,
+    nmX : 0,
+    nmY : 0,
+    brushSize : 0,
+    drawing : false,
+}
+
+ctx.lineCap = "round";
+
+var brushSize = document.querySelector(".bSize");
+var bsOut = document.querySelector(".bSizeOut");
+brushSize.oninput = function() {
+    bsOut.textContent = brushSize.value;
+  }
+
+ctx.strokeStyle = 'hsl(`+ ${meow} +`,100%,50%)';
+addEventListener("pointerup", function(event){
+mouse.drawing = false;
+ctx.closePath();
+});
+
+
+addEventListener("pointerdown", function(event){
+    mouse.drawing = true;
+    ctx.beginPath();
+    mouse.mX = event.layerX;
+    mouse.mY = event.layerY;
+    ctx.lineWidth = brushSize.value;
+});
+ 
+addEventListener("pointermove", function(event){
+    if(mouse.drawing){
+        ctx.beginPath()
+        ctx.strokeStyle = 'hsl('+ meow +',100%,50%)';
+            ctx.moveTo(mouse.mX, mouse.mY);
+            ctx.lineTo(event.layerX, event.layerY);
+            ctx.stroke();
+            mouse.mX = event.layerX;
+            mouse.mY = event.layerY;
+            ctx.closePath();
+            meow++;
+            if(meow >=360) meow = 0;
+    }
+});
+
+const clear = [];
+
+addEventListener("keydown", function(event){
+    if(event.code == "KeyR"){
+        ctx.fillRect(0,0,canvas.width, canvas.height);
+    }
+})
 
 function rgbToHSL(r, g, b) {
     r /= 255;
@@ -44,62 +105,3 @@ function rgbToHSL(r, g, b) {
     return [hue, sat, light];
     
     }
-
-const mouse = {
-    mX : 0,
-    mY : 0,
-    nmX : 0,
-    nmY : 0,
-    drawing : false,
-}
-
-
-
-var nowStyle = 0;
-
-
-
-
-
-
-ctx.lineWidth = 4;
-
-
-ctx.strokeStyle = 'hsl(`+ ${meow} +`,100%,50%)';
-addEventListener("pointerup", function(event){
-mouse.drawing = false;
-ctx.closePath();
-});
-
-
-addEventListener("pointerdown", function(event){
-    mouse.drawing = true;
-    ctx.beginPath();
-    mouse.mX = event.layerX;
-    mouse.mY = event.layerY;
-});
- 
-addEventListener("pointermove", function(event){
-    if(mouse.drawing){
-        ctx.beginPath()
-        ctx.strokeStyle = 'hsl('+ meow +',100%,50%)';
-            ctx.moveTo(mouse.mX, mouse.mY);
-            ctx.lineTo(event.layerX, event.layerY);
-            ctx.stroke();
-            mouse.mX = event.layerX;
-            mouse.mY = event.layerY;
-            ctx.closePath();
-            meow++;
-            if(meow >=360) meow = 0;
-    }
-});
-
-const clear = [];
-
-addEventListener("keydown", function(event){
-    if(event.code == "KeyR"){
-        ctx.fillRect(0,0,canvas.width, canvas.height);
-    }
-})
-
-
