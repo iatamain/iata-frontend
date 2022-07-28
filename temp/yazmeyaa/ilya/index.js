12e8d7cb2a61ed823fd077dba00916989c4f883c
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-07-28 14:33:19 +0300

    added endline character

--- temp/yazmeyaa/ilya/index.js
+++ temp/yazmeyaa/ilya/index.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('button')
-if(button instanceof HTMLButtonElement === false){
+if (button instanceof HTMLButtonElement === false) {
     throw new Error('button is not a button')
 }
 const text = document.getElementById('text')
@@ -16,7 +16,7 @@ function writeTextWithInterval(
 ) {
     if (typeof string !== 'string' || !text) throw new Error('Wrong function arguments')
     if (runningIntervalFlag === true) return undefined
-    
+
 
     let currentIndex = 0
     const interval = setInterval(() => {
@@ -25,7 +25,7 @@ function writeTextWithInterval(
         if (text.innerHTML.length >= string.length) {
             clearInterval(interval)
             runningIntervalFlag = false
-            if(button instanceof HTMLButtonElement) button.disabled = false            
+            if (button instanceof HTMLButtonElement) button.disabled = false
         }
     }, timer)
 }
@@ -34,12 +34,12 @@ function buttonEvent() {
     if (!text) {
         throw new Error('TEXT element is not defined')
     }
-    if(!runningIntervalFlag){
+    if (!runningIntervalFlag) {
         text.innerHTML = ''
         writeTextWithInterval(stringToWrite, 75)
         runningIntervalFlag = true
     }
-    if(button instanceof HTMLButtonElement){
+    if (button instanceof HTMLButtonElement) {
         button.disabled = runningIntervalFlag
     }
 }
