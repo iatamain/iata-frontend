d50422fbd9e6dd94ccdf9ceda9be59d9617fdd32
Volaheck <tangerinene@gmail.com>
2024-04-16 22:17:23 +0300

    move old iata to the projects folder

--- temp/Laplas/frontend/src/components/lessons/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './style.css'
-
-const Lessons = ()=>(
-    <span>Индивидуальные занятия</span>
-)
-export { Lessons }
--- temp/Laplas/frontend/src/components/notfound/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './style.css'
-
-const NotFound = ()=>(
-    <span>Тут ничего нет</span>
-)
-export { NotFound }
